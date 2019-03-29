991e7c50fdab4aee9c216267bd13b7afd12b8602
nullromo <kylekovacs@berkeley.edu>
2019-03-29 13:01:33 -0700

    cleaned up code

--- src/main/scala/example/CompressionAccelerator.scala
+++ src/main/scala/example/CompressionAccelerator.scala
@@ -20,8 +20,9 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
   //          sIdle: waiting for a RoCC command
   //  sLookForMatch: searching for a 4-byte match
   //   sEmitLiteral: feed the literal opcode and data to the output
+  //      sEmitCopy: emit copy tags until there is no repeated data
   // sEmitRemainder: emit the end of a stream as a literal
-  val sIdle :: sLookForMatch :: sEmitLiteral :: sEmitRemainder :: Nil = Enum(4)
+  val sIdle :: sLookForMatch :: sEmitLiteral :: sEmitCopy :: sEmitRemainder :: Nil = Enum(5)
   val state = RegInit(sIdle)
 
   // get the RoCC command
@@ -32,25 +33,24 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
   val doUncompress: Bool = cmd.bits.inst.funct === 1.U
   val doSetLength: Bool = cmd.bits.inst.funct === 2.U
 
-  // register to hold the length field given by the setLength command
+  // hold the length field given by the setLength command
   val length = RegInit(0.U(32.W))
 
-  // registers to hold the source and destination addresses for the compress and uncompress commands
+  // hold the source and destination addresses for the compress and uncompress commands
   val src = RegInit(0.U(32.W))
   val dst = RegInit(0.U(32.W))
 
-  // register that drives the busy signal to tell the CPU that the accelerator is busy
+  // drives the busy signal to tell the CPU that the accelerator is busy
   val busy = RegInit(false.B)
 
-  // tableSize-entry table, data is 16 bits for offset and 32 bits for value
-  val hashTable = Module(BypassEnableMem(BypassEnableMemParameters(tableSize, 48, syncRead = false, bypass = false)))
+  // tableSize-entry table, data is 16 bits for offset
+  val hashTable = Module(BypassEnableMem(BypassEnableMemParameters(tableSize, 16, syncRead = false, bypass = false)))
   hashTable.io.writeEnable := false.B
   hashTable.io.writeAddress := 0.U
   hashTable.io.writeData := 0.U
   hashTable.io.readAddress := 0.U
 
   // components for state machine
-  val ip        = RegInit( 0.U(32.W))
   val shift: UInt = (32 - log2Floor(tableSize)).U
   val ip_end    = RegInit( 0.U(32.W))
   val base_ip   = RegInit( 0.U(32.W))
@@ -61,13 +61,11 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
   val skip      = RegInit(32.U(32.W))
   val next_ip   = RegInit( 0.U(32.W))
   val candidate = RegInit( 0.U(32.W))
-  val hash      = RegInit( 0.U(32.W))
   val bbhl      = RegInit( 0.U(32.W))
   val op        = RegInit( 0.U(32.W))
   val n         = RegInit( 0.U(32.W))
   val base      = RegInit( 0.U(32.W))
   val count     = RegInit( 0.U(32.W))
-  dontTouch(ip)
   dontTouch(ip_end)
   dontTouch(base_ip)
   dontTouch(next_emit)
@@ -76,16 +74,14 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
   dontTouch(skip)
   dontTouch(next_ip)
   dontTouch(candidate)
-  dontTouch(hash)
   dontTouch(bbhl)
   dontTouch(op)
+  dontTouch(n)
   dontTouch(base)
   dontTouch(count)
 
   // state machine
   when(state === sLookForMatch) {
-    ip := next_ip
-    hash := next_hash
     bbhl := (skip >> 5.U).asUInt()
     skip := skip + (skip >> 5.U).asUInt()
     next_ip := next_ip + (skip >> 5.U).asUInt()
@@ -104,9 +100,6 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
 
       //TODO: figure out how to access the memory
 //    when(io.mem(next_ip) === io.mem(candidate)) {
-//      state := sEmitLiteral
-//    }
-
       // pretend mem(7) === mem(0) TODO: [remove me]
       when(next_ip === 7.U) {
         state := sEmitLiteral
@@ -152,7 +145,6 @@ class CompressionAcceleratorModule(outer: CompressionAccelerator)(implicit p: Pa
         busy := true.B
         src := cmd.bits.rs1
         dst := cmd.bits.rs2
-        ip := cmd.bits.rs1 + 1.U
         ip_end := cmd.bits.rs1 + length
         base_ip := cmd.bits.rs1
         next_emit := cmd.bits.rs1
