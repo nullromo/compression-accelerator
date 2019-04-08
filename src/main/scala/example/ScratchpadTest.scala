2c29cff63e0199de8b6625798a6f9bc9a252ab0f
nullromo <kylekovacs@berkeley.edu>
2019-04-08 12:24:38 -0700

    Updated gitignore

--- src/main/scala/example/ScratchpadTest.scala
+++ src/main/scala/example/ScratchpadTest.scala
@@ -6,13 +6,10 @@ import freechips.rocketchip.devices.tilelink.TLTestRAM
 import freechips.rocketchip.tile.{LazyRoCC, LazyRoCCModuleImp, OpcodeSet}
 
 class ScratchpadTest(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes) {
-
   override lazy val module = new ScratchpadTestModule(this)
-
 }
 
 class ScratchpadTestModule(outer: ScratchpadTest)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
-
   val ram = LazyModule(new TLTestRAM(AddressSet(0, 0xFFFF), beatBytes = 8))
   val accelerator = LazyModule(new CompressionAccelerator(outer.opcodes))
 
