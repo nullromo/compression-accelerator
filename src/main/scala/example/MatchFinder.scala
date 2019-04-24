35b6ea7d22deebcf2b84788f3877fa874a31115c
nullromo <kylekovacs@berkeley.edu>
2019-04-24 00:11:56 -0700

    Fixed MatchFinder to use external matchB pointer.

--- src/main/scala/example/MatchFinder.scala
+++ src/main/scala/example/MatchFinder.scala
@@ -5,133 +5,111 @@ import chisel3.core.dontTouch
 import chisel3.util.Decoupled
 import chisel3.util.experimental.loadMemoryFromFile
 
-class MatchFoundOutput(addressWidth: Int) extends Bundle {
-  val matchA = Output(UInt(addressWidth.W))
-  val matchB = Output(UInt(addressWidth.W))
-
-  override def cloneType: this.type = new MatchFoundOutput(addressWidth).asInstanceOf[this.type]
-}
-
 class MatchFinderIO(dataWidth: Int, addressWidth: Int) extends Bundle {
-  //ready/valid IO for producing the output
-  val out = Decoupled(new MatchFoundOutput(addressWidth))
-  // ready/valid IO for accepting the starting arguments
-  val start = Flipped(Decoupled(UInt(addressWidth.W)))
-  // ready/valid IO for scratchpad address
-  val memoryReadAddress = Decoupled(UInt(addressWidth.W))
-  // ready/valid IO for scratchpad data
-  val newCandidateData = Flipped(Decoupled(UInt(dataWidth.W)))
-  // global base pointer connection
-  val src = Input(UInt(addressWidth.W))
-  // reset the hash table
-  val clear = Input(Bool())
+    //ready/valid IO for producing the output
+    val matchA = Decoupled(UInt(addressWidth.W))
+    // ready/valid IO for accepting the starting arguments
+    val start = Flipped(Decoupled(UInt(addressWidth.W)))
+    // ready/valid IO for scratchpad address
+    val matchB = Input(UInt(addressWidth.W))
+    // ready/valid IO for scratchpad data
+    val newCandidateData = Flipped(Decoupled(UInt(dataWidth.W)))
+    // global base pointer connection
+    val src = Input(UInt(addressWidth.W))
+    // reset the hash table
+    val clear = Input(Bool())
 
-  override def cloneType: this.type = new MatchFinderIO(dataWidth, addressWidth).asInstanceOf[this.type]
+    override def cloneType: this.type = new MatchFinderIO(dataWidth, addressWidth).asInstanceOf[this.type]
 }
 
 class MatchFinder(dataWidth: Int, addressWidth: Int, hashTableSize: Int) extends Module {
-  val io = IO(new MatchFinderIO(dataWidth, addressWidth))
+    val io = IO(new MatchFinderIO(dataWidth, addressWidth))
 
-  // true when looking for a match
-  val looking = RegInit(false.B)
+    // true when looking for a match
+    val looking = RegInit(false.B)
 
-  // pointers given at the start of the search
-  val matchPointer = RegInit(0.U(addressWidth.W))
+    // when start looking, save the base pointer and set the state to looking
+    when(io.start.fire()) {
+        looking := true.B
+    }
 
-  //TODO: add skip and bbhl for optimization
-  //TODO: add update of the previous thing in the hash table after a copy is emitted for optimization
+    // when the output is accepted, we are done with the looking state and ready to start again
+    when(io.matchA.fire()) {
+        looking := false.B
+    }
 
-  // ready to start when we are not currently looking
-  io.start.ready := !looking
+    //TODO: add skip and bbhl for optimization
+    //TODO: add update of the previous thing in the hash table after a copy is emitted for optimization
 
-  // always ready to receive data from the memory
-  io.newCandidateData.ready := true.B
+    // ready to start when we are not currently looking
+    io.start.ready := !looking
 
-  // when start looking, save the base pointer and set the state to looking
-  when(io.start.fire()) {
-    looking := true.B
-    matchPointer := io.start.bits
-  }.otherwise {
-    // advance the searching pointer
-    matchPointer := matchPointer + 1.U
-  }
+    // always ready to receive data from the memory
+    io.newCandidateData.ready := true.B
 
-  // we want to read the scratchpad at the matchPointer location
-  io.memoryReadAddress.bits := matchPointer
+    // create the hash table
+    val hashTable = Module(new HashTable(32, 16, hashTableSize))
 
-  // the address sent to the memory is always valid
-  io.memoryReadAddress.valid := true.B
+    // clear the present bits in the hash table when a brand new compression job is starting
+    hashTable.io.clearPresent := io.clear
 
-  // create the hash table
-  val hashTable = Module(new HashTable(32, 16, hashTableSize))
+    // update the hash table with the new data and the offset of the new data
+    hashTable.io.newData := io.newCandidateData.bits
+    hashTable.io.newOffset := io.matchB - io.src
 
-  // clear the present bits in the hash table when a brand new compression job is starting
-  hashTable.io.clearPresent := io.clear
+    // true when a match has been found
+    val matchFound: Bool = Wire(Bool())
 
-  // update the hash table with the new data and the offset of the new data
-  hashTable.io.newData := io.newCandidateData.bits
-  hashTable.io.newOffset := matchPointer - io.src
+    matchFound := hashTable.io.oldPresent &&
+        (hashTable.io.oldData === io.newCandidateData.bits) &&
+        io.newCandidateData.valid &&
+        io.matchA.bits =/= io.matchB
 
-  // true when a match has been found
-  val matchFound: Bool = Wire(Bool())
+    // only write to the hash table when we are looking or starting to look
+    hashTable.io.enable := io.newCandidateData.valid && (looking || io.start.fire())
 
-  matchFound := hashTable.io.oldPresent &&
-    (hashTable.io.oldData === io.newCandidateData.bits) &&
-    io.newCandidateData.valid &&
-    io.out.bits.matchA =/= io.out.bits.matchB
+    // we have valid output when we are still in the looking state and there is a match
+    io.matchA.valid := looking && matchFound
 
-  // only write to the hash table when we are looking or starting to look
-  hashTable.io.enable := io.newCandidateData.valid && (looking || io.start.fire())
-
-  // we have valid output when we are still in the looking state and there is a match
-  io.out.valid := looking && matchFound
-
-  // when the output is accepted, we are done with the looking state and ready to start again
-  when(io.out.fire() && looking) {
-    looking := false.B
-  }
-
-  // the beginning of the found match is the old offset for this data (the last location that held the same data)
-  io.out.bits.matchA := hashTable.io.oldOffset + io.src
-
-  // the end of the found match is the place we are looking at
-  io.out.bits.matchB := matchPointer
+    // the beginning of the found match is the old offset for this data (the last location that held the same data)
+    io.matchA.bits := hashTable.io.oldOffset + io.src
 }
 
 class MatchFinderTestModule(memDataWidth: Int, dataWidth: Int, addressWidth: Int, hashTableSize: Int) extends Module {
-  // use the same IO as the matchFinder
-  val io = IO(new MatchFinderIO(dataWidth, addressWidth))
+    // use the same IO as the matchFinder
+    val io = IO(new MatchFinderIO(dataWidth, addressWidth))
 
-  // create a matchFinder
-  val matchFinder = Module(new MatchFinder(dataWidth: Int, addressWidth: Int, hashTableSize: Int))
+    // create a matchFinder
+    val matchFinder = Module(new MatchFinder(dataWidth: Int, addressWidth: Int, hashTableSize: Int))
 
-  // we don't need to clear the hash table
-  matchFinder.io.clear := false.B
+    // we don't need to clear the hash table
+    matchFinder.io.clear := false.B
 
-  // create a memory
-  val mem = Mem(4096, UInt(memDataWidth.W))
-  loadMemoryFromFile(mem, "data/alignerTestData.txt")
+    // create a memory
+    val mem = Mem(4096, UInt(memDataWidth.W))
+    loadMemoryFromFile(mem, "data/alignerTestData.txt")
 
-  // create memory aligner adapter
-  //TODO: this shouldn't depend on the memory aligner... it doesn't matter that much though as long as things work
-  val aligner = Module(new MemoryReadAligner(
-    32, 32, 32, 64
-  ))
+    // create memory aligner adapter
+    //TODO: this shouldn't depend on the memory aligner... it doesn't matter that much though as long as things work
+    val aligner = Module(new MemoryReadAligner(
+        32, 32, 32, 64
+    ))
 
-  // connect aligner to memory
-  aligner.io.memIO.data := mem.read(aligner.io.memIO.address)
+    // connect aligner to memory
+    aligner.io.memIO.data := mem.read(aligner.io.memIO.address)
 
-  // connect the matchFinder to the read aligner
-  aligner.io.readIO.address <> matchFinder.io.memoryReadAddress
-  matchFinder.io.newCandidateData <> aligner.io.readIO.data
+    // connect the matchFinder to the read aligner
+    aligner.io.readIO.address.bits := io.matchB
+    aligner.io.readIO.address.valid := true.B
+    matchFinder.io.newCandidateData <> aligner.io.readIO.data
 
-  // pass the rest of the IO through
-  matchFinder.io.start <> io.start
-  io.out <> matchFinder.io.out
-  io.memoryReadAddress <> DontCare
-  io.newCandidateData <> DontCare
-  matchFinder.io.src := io.src
+    // pass the rest of the IO through
+    matchFinder.io.start <> io.start
+    io.matchA <> matchFinder.io.matchA
+    io.newCandidateData <> DontCare
+    matchFinder.io.src := io.src
+    matchFinder.io.matchB := io.matchB
 
-  dontTouch(aligner.io)
-  dontTouch(matchFinder.io)
+    dontTouch(aligner.io)
+    dontTouch(matchFinder.io)
 }
--- src/test/scala/example/MatchFinderTester.scala
+++ src/test/scala/example/MatchFinderTester.scala
@@ -3,70 +3,68 @@ package example
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 class MatchFinderTester(c: MatchFinderTestModule) extends PeekPokeTester(c) {
-  // keep track of test cycles
-  var cycles = 0
+    // keep track of test cycles
+    var cycles = 0
 
-  // hold all the matches found by the module
-  var matchesFound: Array[(BigInt, BigInt)] = Array[(BigInt, BigInt)]()
+    // hold all the matches found by the module
+    var matchesFound: Array[(BigInt, BigInt)] = Array[(BigInt, BigInt)]()
 
-  // always be ready for output and always send valid input
-  poke(c.io.out.ready, true)
-  poke(c.io.start.valid, true)
+    // always be ready for output and always send valid input
+    poke(c.io.matchA.ready, true)
+    poke(c.io.start.valid, true)
 
-  // set global base
-  poke(c.io.src, 0)
+    // set global base
+    poke(c.io.src, 0)
 
-  // keep track of pointers
-  var base: BigInt = 0
-  var end: BigInt = 0
+    // keep track of pointers
+    var base: BigInt = 0
+    var end: BigInt = 0
+    var matchB = 0
 
-  // count matches found
-  var matches = 0
+    // count matches found
+    var matches = 0
 
-  // find matches
-  while (base < 69 && cycles < 10000) {
+    // find matches
+    while (base < 69 && cycles < 1000) {
+        poke(c.io.matchB, matchB)
+        // send in the input
+        poke(c.io.start.bits, base)
 
-    // send in the input
-    poke(c.io.start.bits, base)
-    if(cycles % 100 == 0)
-      poke(c.io.start.valid, true)
-    else
-    poke(c.io.start.valid, false)
+        // if the output is valid, deal with it
+        if (peek(c.io.matchA.valid) != 0) {
+            base = peek(c.io.matchA.bits)
+            end = matchB
+            matches += 1
+            println("match found: " + base + " to " + end)
+            matchesFound = matchesFound :+ (base, end)
+            base = end + 1
+        }
 
-    // if the output is valid, deal with it
-    if (peek(c.io.out.valid) != 0) {
-      base = peek(c.io.out.bits.matchA)
-      end = peek(c.io.out.bits.matchB)
-      matches += 1
-      println("match found: " + base + " to " + end)
-      matchesFound = matchesFound :+ (base, end)
-      base = end + 1
+        s(1)
     }
 
-    s(1)
-  }
+    println(matches + " matches found.")
 
-  println(matches + " matches found.")
+    // expected matches based on file
+    val expectedMatches = Array(
+        (0, 44), (20, 57), (16, 70)
+    )
 
-  // expected matches based on file
-  val expectedMatches = Array(
-    (0, 44), (20, 57), (16, 70)
-  )
+    expect(expectedMatches sameElements matchesFound, "Wrong matches :(")
 
-  expect(expectedMatches sameElements matchesFound, "Wrong matches :(")
-
-  def s(n: Int): Unit = {
-    step(n)
-    cycles += n
-  }
+    def s(n: Int): Unit = {
+        step(n)
+        cycles += n
+        matchB += 1
+    }
 }
 
 class MatchFinderSpec extends ChiselFlatSpec {
-  val dutGen: () => MatchFinderTestModule = () => new MatchFinderTestModule(
-    64, 32, 32, 512)
-  "MatchFinder" should "find matches" in {
-    Driver.execute(TesterArgs() :+ "MatchFinder", dutGen) {
-      c => new MatchFinderTester(c)
-    } should be(true)
-  }
+    val dutGen: () => MatchFinderTestModule = () => new MatchFinderTestModule(
+        64, 32, 32, 512)
+    "MatchFinder" should "find matches" in {
+        Driver.execute(TesterArgs() :+ "MatchFinder", dutGen) {
+            c => new MatchFinderTester(c)
+        } should be(true)
+    }
 }
